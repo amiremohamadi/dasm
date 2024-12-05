887c9060373e6a2129eec7e0662e5d6a3701199e
Amir Mohammadi <amiremohamadi@yahoo.com>
2024-12-05 14:41:17 +0330

    doing stuffs using cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,44 @@
 mod lexer;
 mod parser;
 
+use std::env;
+use std::fs;
+
+use lexer::Lexer;
+use parser::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let mut tokens = Vec::new();
+    let args = env::args().collect::<Vec<String>>();
+
+    if args.len() < 3 {
+        println!("usage: ./dasm filename.s <mode> (<mode>: --lex, --parse)");
+        return;
+    }
+
+    let filename = &args[1];
+    let mode = &args[2];
+
+    if let Ok(buf) = fs::read(filename) {
+        let buf = String::from_utf8_lossy(&buf);
+        let mut lexer = Lexer::new(buf.into_owned());
+
+        tokens = lexer.tokenize();
+    }
+
+    match mode.as_str() {
+        "--lex" => {
+            for token in tokens {
+                println!("{:?}", token);
+            }
+        }
+        "--parse" => {
+            let mut parser = Parser::new(tokens);
+            let insts = parser.parse();
+            for inst in insts {
+                println!("{:?}", inst);
+            }
+        }
+        _ => {}
+    }
 }
