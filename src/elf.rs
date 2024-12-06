0cc0bd5e22be84f2c8ac1b7f4298f68c5cf45f4a
Amir Mohammadi <amiremohamadi@yahoo.com>
2024-12-06 22:06:37 +0330

    messy workaround on elf64

--- src/elf.rs
+++ src/elf.rs
@@ -109,7 +109,7 @@ fn create_text_section() -> SectionHeader {
 
 fn create_strtab_section() -> SectionHeader {
     let mut header = SectionHeader::default();
-    header.name = 0x02;
+    header.name = 0x07;
     header.r#type = 0x03; // STRTAB
     header.address_align = 0x01;
     header
@@ -117,8 +117,8 @@ fn create_strtab_section() -> SectionHeader {
 
 fn create_symtable_section() -> SectionHeader {
     let mut header = SectionHeader::default();
-    header.name = 0x03;
-    header.r#type = 0x02; // SYMTAB
+    header.name = 17;
+    header.r#type = 2; // SYMTAB
     header.address_align = 0x08;
     header
 }
@@ -132,12 +132,12 @@ pub fn generate_elf64() {
     let null_section_header = create_null_section();
     buf.extend_from_slice(&null_section_header.to_bytes());
 
-    let data = b"\0.text\0.shstrtab\0";
+    let data = b"\0.text\0.shstrtab\0.symtab\0.strtab\0";
 
     let mut text_section_header = create_text_section();
     text_section_header.offset =
         size_of::<Header>() as u64 + (size_of::<SectionHeader>() as u64 * 5) + data.len() as u64;
-    text_section_header.size = 2;
+    text_section_header.size = 5;
     buf.extend_from_slice(&text_section_header.to_bytes());
 
     let mut shstrtab_section_header = create_strtab_section();
@@ -150,30 +150,36 @@ pub fn generate_elf64() {
     symtab_section_header.info = 1;
     symtab_section_header.link = 4;
     symtab_section_header.entry_size = 24;
-    symtab_section_header.size = 24;
+    symtab_section_header.size = 48;
     symtab_section_header.offset = size_of::<Header>() as u64
         + (size_of::<SectionHeader>() as u64 * 5)
         + data.len() as u64
-        + b"11".len() as u64;
+        + b"\xb8\x09\x00\x00\x00".len() as u64;
     buf.extend_from_slice(&symtab_section_header.to_bytes());
 
     let mut strstab_section_header = create_strtab_section();
-    strstab_section_header.name = 4;
+    strstab_section_header.name = 25;
     strstab_section_header.offset = size_of::<Header>() as u64
         + (size_of::<SectionHeader>() as u64 * 5)
         + data.len() as u64
-        + b"11".len() as u64
-        + 24;
-    strstab_section_header.size = b"_start\0".len() as _;
+        + b"\xb8\x09\x00\x00\x00".len() as u64
+        + 48;
+    strstab_section_header.size = b".text\0_start\0".len() as _;
     buf.extend_from_slice(&strstab_section_header.to_bytes());
 
     buf.extend_from_slice(data);
-    buf.extend_from_slice(b"11");
+    buf.extend_from_slice(b"\xb8\x09\x00\x00\x00");
+
     buf.extend_from_slice(&[
-        00, 00, 00, 00, 0x10, 00, 0x1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
+        00, 00, 00, 00, 3, 00, 0x1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
         00, 00,
     ]);
-    buf.extend_from_slice(b"_start\0");
+    buf.extend_from_slice(&[
+        06, 00, 00, 00, 0x10, 00, 0x1, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00,
+        00, 00,
+    ]);
+
+    buf.extend_from_slice(b".text\0_start\0");
 
     println!("{:?}", buf);
 }
