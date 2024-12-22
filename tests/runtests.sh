44306c61b8ec600b209e13777adf762ab2e1cdad
Amir Mohammadi <amiremohamadi@yahoo.com>
2024-12-22 17:44:55 +0330

    verbose test stages

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -3,8 +3,13 @@
 for test in $(ls ./tests/*.s); do
     name=$(basename "$test")
     name="${name%.s}"
+
+    echo -e "\033[32mRunning --lex for $test\033[0m"
     ./dasm $test --lex > ./tests/$name.lex
+
+    echo -e "\033[32mRunning --elf for $test\033[0m"
     ./dasm $test --elf ./tests/$name.o
 done
 
-git --no-pager diff ./tests/
+git --no-pager diff --exit-code ./tests/ || exit 1
+git --no-pager diff --cached --exit-code ./tests/ || exit 1
